7e2a968a8c985f852af787194cce2b872337772c
hyunmu <kcpark.kr@gmail.com>
2020-03-29 20:48:37 +0900

    Initial commit

---
+++ PageSpider/utilities/database_utilities.py
@@ -0,0 +1,8 @@
+def create_databae(database_path: str):
+    # TODO
+    pass
+
+
+def save_words_to_database(database_path: str, words_list: str):
+    # TODO
+    pass
---
+++ PageSpider/utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(url_path: str):
+    # TODO
+    pass
+
+def load_page(url: str):
+    # TODO
+    pass
+
+
+def scrape_page(page_contents: str):
+    # TODO
+    pass
